704588e3a9de9bbbe8593b2f5f6dfb9283724386
karpeomkar608 <karpeomkar608@gmail.com>
2025-04-29 15:15:07 +0530

    fund

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://ipoanalyser.in/',
+    generateRobotsTxt: true, // (optional) will create robots.txt file too
+    sitemapSize: 7000,        // optional, for big sites
+  };
+  
